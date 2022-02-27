e946f0f61bdfa31bd5e066fbb43e3497ddc3e3fd
Ashish Chaudhary <68781036+cookieSensei@users.noreply.github.com>
2022-02-27 21:53:41 +0530

    Update UI and added a reactive variable
    Reactive variable was added to prevent copying of data twice

--- 1 - LinearRegression/CompleteLinearRegressionApp.R
+++ 1 - LinearRegression/CompleteLinearRegressionApp.R
@@ -1,49 +1,87 @@
+# Install the required libraries or package 
+
+# Remove the hash # symbol in line 5 and execute it (ctrl + enter on windows or cmd + enter on mac) to install packages
+
+# install.packages(c("shiny", "shinythemes", "dplyr", "ggplot2"))
+
+
+
+
+#Loading essential libraries
+
 library(shiny)
 library(shinythemes)
 library(dplyr)
 library(ggplot2)
 
+# Load up the required dataset
 dirtyDataset <- read.csv("resources/cost_revenue_dirty.csv")
 
+# Check the top rows of dataset
 dirtyDataset %>% head()
 
+# Have a quick look at the dataset. Notics there are a lot of zeroes
+# View(dirtyDataset) #uncomment
+
+# Check column names
 colnames(dirtyDataset)
 
+# Keep only the required columns of data frame
 dirtyDataset <- dirtyDataset[c(3,4,5)]
 
 dirtyDataset %>% head()
 
+
+# Edit column names to our preference in snakeCasing
 colnames(dirtyDataset) <- c("movieTitle", "productionBudget", "worldwideGross")
 
 dirtyDataset %>% head()
 
+
+# Check for missing values
 dirtyDataset[!complete.cases(dirtyDataset)] 
 # That means there are no missing values 
 # but there are a lot of false zeros in our dataset which could hinder our model
 
+# Filter dataset rows where productionBudget is less than 1000
 dirtyDataset[dirtyDataset$productionBudget < 1000, ] 
 # something is wrong maybe these are strings
 
+# Check structure of dataset
 str(dirtyDataset)
 
+# $ is a special symbol in R. to remove it, we use the pattern \\$
+# using \\$ as pattern R will look for the dollar sign $ and replace it with the second argument 
+# the second argument here is an empty string ""
 dirtyDataset$productionBudget <- gsub(pattern = "\\$", "", dirtyDataset$productionBudget)
+
+# We remove commas "," by replacing it with an empty string
 dirtyDataset$productionBudget <- gsub(pattern = ",", "", dirtyDataset$productionBudget)
+
+# Now we can safely convert the remaining string to numeric values
 dirtyDataset$productionBudget <- as.numeric(dirtyDataset$productionBudget)
 
+# Check the head 
 dirtyDataset %>% head()
 
+# Let's do the same for the worldwideGross column
 dirtyDataset$worldwideGross <- gsub(pattern = "\\$", "", dirtyDataset$worldwideGross)
 dirtyDataset$worldwideGross <- gsub(pattern = ",", "", dirtyDataset$worldwideGross)
 dirtyDataset$worldwideGross <- as.numeric(dirtyDataset$worldwideGross)
 
+# Structure
 str(dirtyDataset)
 
+# Check incomplete cases
 dirtyDataset[!complete.cases(dirtyDataset),]
 
+# Keep only the complete cases
 dirtyDataset <- dirtyDataset[complete.cases(dirtyDataset),]
 
+# Check the complete cases
 dirtyDataset[complete.cases(dirtyDataset),]
 
+# Check the incomplete cases
 dirtyDataset[!complete.cases(dirtyDataset),]
 
 dirtyDataset %>% head()
@@ -53,25 +91,36 @@ str(dirtyDataset)
 # new problem arises
 # there are zeros in our data
 
+# Let's check if either of our numeric columns have zeros
+# Filer using the vectorized "or" operator and check the head 
 dirtyDataset[(dirtyDataset$productionBudget < 1000 | dirtyDataset$worldwideGross < 1000), ] %>% head()
 
+# Keep only the rows where productionBudget and worldwideGross, both have more that 1000 value
+# Its just to remove zeros
 dirtyDataset[(dirtyDataset$productionBudget > 1000 & dirtyDataset$worldwideGross > 1000), ] %>% head()
 
+# Save the new output as cleanDataset
 cleanDataset <- dirtyDataset[(dirtyDataset$productionBudget > 1000 & dirtyDataset$worldwideGross > 1000), ]
 
 cleanDataset %>% head()
 
+# Lets check the minimum
 min(cleanDataset$productionBudget)
 
 min(cleanDataset$worldwideGross)
 
+# Check for incomplete rows
 cleanDataset[!complete.cases(cleanDataset),]
 
+# Lets call it finalDataset as our data cleaning is complete
 finalDataset <- cleanDataset
 
+# Remove cleanDataset and dirtyDataset varaibles
 rm(cleanDataset)
 rm(dirtyDataset)
 
+# Lets look at the finalDataset again to see if it is in the format we want it to be
+# Column names have snakeCasing, there are no zeros, or missing values, great
 finalDataset %>% head()
 
 
@@ -84,18 +133,33 @@ finalDataset %>% head()
 
 finalDataset %>% head()
 
+# Lets divide our numeric columns by 1 million 
 finalDataset$productionBudget <- finalDataset$productionBudget / 1e6
 finalDataset$productionBudget
 
 finalDataset$worldwideGross <- finalDataset$worldwideGross / 1e6
 
+
+
+# Lets create a regressor for our data
+# Use the help guide on lm() for check what arguments it uses
+?lm()
+
+# formula = y axis is directly proportional to x axis
 movieReg <- lm(data = finalDataset, formula = worldwideGross ~ productionBudget)
 
+# Check the slope (m) and intercept (y) on our equation y = mx+c
 movieReg
 
+# Now lets predict the y axis using an x axis
+# Here we use productionBudget column and predict what y values (worldwideGross) our regressor movieReg provides
 predictedGross <- predict(movieReg, data = finalDataset$productionBudget)
 
+# Lets do a sample plot first before we head to make a reactive app on it
 
+# Create a scatterplot with productionBudget on x axis and worldwideGross on y axis
+# Later we add a layer of geom_line
+# The line plot has productionBudget on x axis and predicted y values (predictedGross) on y axis
 regressionPlot <- finalDataset %>% ggplot(aes(productionBudget, worldwideGross)) +
     geom_point(color = "blue", alpha = 0.2) +
     geom_line(
@@ -112,20 +176,29 @@ regressionPlot <- finalDataset %>% ggplot(aes(productionBudget, worldwideGross))
     labs(x = "Production budget of movie (in millions)",
          y = "Worldwide Gross (in millions)") 
 
-# regressionPlot
+# Call the plot
+regressionPlot
 
+# Check the stats on regressor
 summary(movieReg)
-
+# The R square value is almost 55% which means our predictions using linear regression are 55% accurate
+# This is a good accuracy given that we are only using budget to predict revenue
+# There are other variables which could enhance our prediction
+# For example - movie production time, actors, place of production, directors and so on
 
 finalDataset %>% head()
 
 
+# Check the number of rows on our dataset
+nrow(finalDataset) # 5023 rows
 
-nrow(finalDataset)
+
+# Look at the tail 
 finalDataset %>% tail()
+# There is a mismatch between number of rows and row index numbers
+# There are 5023 rows but dataset ends with 5383
 
-    # lets reset row names now
-
+# lets reset row names now
 rownames(finalDataset) <- 1:nrow(finalDataset)
 
 nrow(finalDataset)
@@ -133,16 +206,17 @@ finalDataset %>% tail()
 
 
 #######################
+###############  SHINY APP
+#######################
 
 
 
-
-
+# User interface of our app
 ui <- fluidPage(
     
-    theme = shinytheme("united"),
+    theme = shinytheme("united"), # Theme used. We could also use bootstrap themes here.
     
-    tabsetPanel(
+    tabsetPanel( # Hit F1 on function for more info
         
         tabPanel(
             "Reactive plot",
@@ -164,11 +238,9 @@ ui <- fluidPage(
             ),
             
             fluidRow(
-                column(
-                    10,
-                    verbatimTextOutput("brushOutput")
-                )
+                tableOutput("tableBrush")
             )
+            
         ),
         
         tabPanel(
@@ -213,11 +285,14 @@ server <- function(input, output){
         summary(movieReg)
     )
     
+    finalDatasetReactive <- reactive({
+        finalDataset$sel <- selected()
+        finalDataset
+    })
+    
     output$ourPlot <- renderPlot({
         
-        finalDataset$sel <- selected()
-        
-        finalDataset %>% ggplot(aes(productionBudget, worldwideGross)) +
+        finalDatasetReactive() %>% ggplot(aes(productionBudget, worldwideGross)) +
             geom_point(aes(colour = sel), alpha = 0.4) +
             scale_color_discrete(limits = c("TRUE", "FALSE"))+
             geom_line(
@@ -236,10 +311,10 @@ server <- function(input, output){
 
     }, res = 96)
     
-    output$brushOutput <- renderPrint({
-        finalDataset$sel <- selected()
-        finalDataset[finalDataset$sel == "TRUE",]
-        })
+    output$tableBrush <- renderTable({
+        finalDatasetReactive()[finalDatasetReactive()$sel == "TRUE",] %>% select(movieTitle, productionBudget, worldwideGross)
+    })
+
 }
 
 shinyApp(ui, server)
